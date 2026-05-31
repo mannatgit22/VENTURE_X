bc55f39ba3bdb20764c2c46379719d11a70ac4a6
Mannat <mannatmishra04@gmail.com>
2026-05-31 14:50:00 +0530

    cache issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from "react";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
